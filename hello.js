348ec0dda3143bafdac430d8a5948ace579c40ae
Unknown <opticalfalicy@gmail.com>
2019-09-15 09:27:27 -0400

    gradient attempt

---
+++ hello.js
@@ -0,0 +1,7 @@
+function colorGen () {
+  const r = Math.floor (Math.random () * 256);
+  const g = Math.floor (Math.random () * 256);
+  const b = Math.floor (Math.random () * 256);
+  document.getElementById ('color1').style.backgroundColor =
+    'rgb(' + r + ',' + g + ',' + b + ')';
+}
